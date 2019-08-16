9dba7037e6e19a0484f4a11dc4cd246c90fd9f0f
vadimkirov <vadimkirov@yandex.ru>
2019-08-16 09:37:21 +0300

    del and add and update and order all test OK ref

--- template/space/src/main/java/com/space/controller/ShipController.java
+++ template/space/src/main/java/com/space/controller/ShipController.java
@@ -12,7 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
